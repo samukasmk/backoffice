83eae6c68ea98013bf793886b6deb95bb2cb78b0
Samuel Sampaio <samukasmk@gmail.com>
2023-03-06 21:13:36 -0300

    [feature/layout-improvements] Improving django admin relations

--- apps/product/admin.py
+++ apps/product/admin.py
@@ -5,18 +5,23 @@ from apps.product.models import PackingSlipCustomization, ProductPipelineTask, P
 @admin.register(PackingSlipCustomization)
 class PackingSlipCustomizationAdmin(admin.ModelAdmin):
     list_display = ('name', 'code')
-
+    search_fields = ('name',)
 
 @admin.register(ProductPipelineTask)
 class ProductPipelineTaskAdmin(admin.ModelAdmin):
     list_display = ('name', 'code')
+    search_fields = ('name',)
 
 
 @admin.register(ProductType)
 class ProductTypeAdmin(admin.ModelAdmin):
     list_display = ('name',)
+    search_fields = ('name',)
+    autocomplete_fields = ('pipeline_tasks', 'packing_slip_customizations')
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'product_type', 'weight', 'price', 'seller_commission_tax')
+    autocomplete_fields = ('product_type',)
+    search_fields = ('name',)
